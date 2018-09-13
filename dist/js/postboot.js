4eb930cb78fb27452516f477bdb8fe20d1315d97
tarkhov <agtuks@gmail.com>
2018-09-13 20:12:46 +0300

    ScrollSpy tree improvements for dropdown and list group

--- dist/js/postboot.js
+++ dist/js/postboot.js
@@ -57,17 +57,66 @@ if (!('ontouchstart' in document.documentElement)) {
 })(jQuery);
 
 (function ($) {
-  $(window).on('activate.bs.scrollspy', function (event, obj) {
-    var target = obj.relatedTarget;
-    var selector = '[href="' + target + '"], [data-target="' + target + '"]';
+  var DATA_KEY  = 'bs.scrollspy';
+  var EVENT_KEY = '.' + DATA_KEY;
 
-    $('[data-spy="scroll"]').each(function () {
-      var spyTarget = $(this).attr('data-target');
-      var $spyTarget = $(spyTarget);
-      var $item = $spyTarget.find(selector);
-      if ($item) {
-        $spyTarget.find('.show').removeClass('show');
-        $item.parents('.nav-item').addClass('show');
+  var Event = {
+    ACTIVATE : 'activate' + EVENT_KEY
+  };
+
+  var ClassName = {
+    DROPDOWN_ITEM : 'dropdown-item',
+    NAV_LINK      : 'nav-link',
+    SHOW          : 'show'
+  };
+
+  var Selector = {
+    DATA_SPY      : '[data-spy="scroll"]',
+    DROPDOWN_MENU : '.dropdown-menu',
+    DROPDOWNS     : '.dropup, .dropright, .dropdown, .dropleft',
+    NAV_ITEM      : '.nav-item',
+    SHOW          : '.show'
+  };
+
+  $(window).on(Event.ACTIVATE, function (event, obj) {
+    var url = obj.relatedTarget;
+    var selector = '[href="' + url + '"], [data-target="' + url + '"]';
+
+    var $spys = $(Selector.DATA_SPY);
+    if (!$spys.length) {
+      return true;
+    }
+
+    $spys.each(function () {
+      var target = $(this).attr('data-target');
+      if (!target) {
+        return true;
+      }
+
+      var $target = $(target);
+      if (!$target.length) {
+        return true;
+      }
+
+      var $link = $target.find(selector);
+      if (!$link.length) {
+        return true;
+      }
+
+      var $show = $target.find(Selector.SHOW);
+      if ($show.length) {
+        $show.removeClass(ClassName.SHOW);
+      }
+
+      var item = null;
+      if ($link.hasClass(ClassName.DROPDOWN_ITEM)) {
+        item = Selector.DROPDOWN_MENU + ',' + Selector.DROPDOWNS;
+      } else if ($link.hasClass(ClassName.NAV_LINK)) {
+        item = Selector.NAV_ITEM;
+      }
+
+      if (item !== null) {
+        $link.parentsUntil(target, item).addClass(ClassName.SHOW);
       }
     });
   });
